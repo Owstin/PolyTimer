131071b69adbcc96b07382582273b2bf9ede9015
Evan <evanjprichard@gmail.com>
2018-07-18 11:23:33 -0500

    UI overhaul, updated time storage, average calculation, and a bunch of other small things

--- js/main.js
+++ js/main.js
@@ -3,6 +3,57 @@ var ready = document.getElementById('ready');
 var scramble = document.getElementById('scramble');
 
 var watch = new Timer(timer);
+var ready = false;
+
+var settingsOpen = false;
+
+//Converts the timer.js output strings into milliseconds for math purposes
+function toMS(str) {
+    if(str.includes(":")) {
+        const [mins, secms] = str.split(":");
+        const sec = parseFloat(secms);
+        return ((+mins * 60) + sec) * 1000;
+    } else {
+        return parseFloat(str) * 1000;
+    }
+}
+
+function timeFormatter(s) {
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    ms = Math.round(ms/10);
+
+    if (hrs > 0) {
+
+    } else {
+        if (mins > 0) {
+            if (secs < 9) {
+                if (ms < 9) {
+                    return mins + ':0' + secs + '.0' + ms;
+                } else {
+                    return mins + ':0' + secs + '.' + ms;
+                }
+            } else {
+                if (ms < 9) {
+                    return mins + ':' + secs + '.0' + ms;
+                } else {
+                    return mins + ':' + secs + '.' + ms;
+                }
+            }
+        } else {
+            if (ms < 9) {
+                return secs + '.0' + ms;
+            } else {
+                return secs + '.' + ms;
+            }
+        }
+    }
+}
 
 if (localStorage.getItem("scrambleType") === null) {
     var scrambleType = "333";
@@ -35,21 +86,30 @@ document.body.onkeyup = function(e) {
     if (e.keyCode == 32) {
         if (watch.isOn) {
             watch.stop();
-            timer.style.color = 'white';
             
-            if (watch.minutes < 0) {
-                var floatTime = parseFloat(formattedTime);
-            } else {
-                var floatTime = formattedTime;
+            if (getActiveStyleSheet() == "default") {
+                timer.style.color = 'white';
             }
 
-            solves.push(floatTime);
+            if (getActiveStyleSheet() == "light") {
+                timer.style.color = "black";
+            }
 
-            var row = table.insertRow(2);
+            var msTime = toMS(formattedTime);
+            var fixedTime = timeFormatter(msTime);
+            solves.push(msTime);
+
+            var row = table.insertRow(1);
             var col1 = row.insertCell(0);
             var col2 = row.insertCell(1);
             var col3 = row.insertCell(2);
             var col4 = row.insertCell(3);
+
+            row.id = "solvesTr";
+            col1.id = "solvesTd";
+            col2.id = "solvesTd";
+            col3.id = "solvesTd";
+            col4.id = "solvesTd";
 
             var total = 0;
             var worstSavg = Math.max.apply(Math,solves);
@@ -73,7 +133,7 @@ document.body.onkeyup = function(e) {
                     sumOf5 += last5[i];
                 }
 
-                ao5 = (sumOf5-ao5Remv) / 3; //current average of 5 (sum of last 5 solves minus best and worst divided by 3)
+                ao5 = Math.round((sumOf5-ao5Remv) / 3); //current average of 5 (sum of last 5 solves minus best and worst divided by 3)
 
             } else ao5 = 0.00;
 
@@ -88,16 +148,18 @@ document.body.onkeyup = function(e) {
                     sumOf12 += last12[i];
                 }
 
-                ao12 = (sumOf12-ao12Remv) / 10; //current average of 12 (sum of last 12 solves minus best and worst divided by 10)
+                ao12 = Math.round((sumOf12-ao12Remv) / 10); //current average of 12 (sum of last 12 solves minus best and worst divided by 10)
             } else ao12 = 0.00;
 
             col1.innerHTML = solves.length;
-            col2.innerHTML = floatTime.toFixed(2);
-            col3.innerHTML = ao5.toFixed(2);
-            col4.innerHTML = ao12.toFixed(2);
+            col2.innerHTML = parseFloat(timeFormatter(msTime)).toFixed(2);
+            col3.innerHTML = parseFloat(timeFormatter(ao5)).toFixed(2);
+            col4.innerHTML = parseFloat(timeFormatter(ao12)).toFixed(2);
 
-            ao5s.push(ao5.toFixed(2));
-            ao12s.push(ao12.toFixed(2));
+            console.log(solves);
+
+            ao5s.push(ao5);
+            ao12s.push(ao12);
 
             localStorage.setItem("solves", JSON.stringify(solves));
             localStorage.setItem("ao5s", JSON.stringify(ao5s));
@@ -105,23 +167,28 @@ document.body.onkeyup = function(e) {
 
             localStorage.setItem("scrambleType", JSON.stringify(scrambleType));
         } else {
-            if (settingsOpen == false) {
-                watch.start();
+            watch.start();
+
+            if (getActiveStyleSheet() == "default") {
                 timer.style.color = 'white';
-                
-                if (scrambleType == "222") {var generatedScramble = scramblers["222"].getRandomScramble().scramble_string;}
-                if (scrambleType == "333") {var generatedScramble = scramblers["333"].getRandomScramble().scramble_string;}
-                if (scrambleType == "444") {var generatedScramble = scramblers["444"].getRandomScramble().scramble_string;}
-                if (scrambleType == "555") {var generatedScramble = scramblers["555"].getRandomScramble().scramble_string;}
-                if (scrambleType == "666") {var generatedScramble = scramblers["666"].getRandomScramble().scramble_string;}
-                if (scrambleType == "777") {var generatedScramble = scramblers["777"].getRandomScramble().scramble_string;}
-                
-                if (scrambleType == "333bf") {var generatedScramble = scramblers["333bf"].getRandomScramble().scramble_string;}
-                
-                scrambles.push(generatedScramble);
-                scramble.textContent = generatedScramble;
-                scrambleIndex += 1;
             }
+
+            if (getActiveStyleSheet() == "light") {
+                timer.style.color = "black";
+            }
+            
+            if (scrambleType == "222") {var generatedScramble = scramblers["222"].getRandomScramble().scramble_string;}
+            if (scrambleType == "333") {var generatedScramble = scramblers["333"].getRandomScramble().scramble_string;}
+            if (scrambleType == "444") {var generatedScramble = scramblers["444"].getRandomScramble().scramble_string;}
+            if (scrambleType == "555") {var generatedScramble = scramblers["555"].getRandomScramble().scramble_string;}
+            if (scrambleType == "666") {var generatedScramble = scramblers["666"].getRandomScramble().scramble_string;}
+            if (scrambleType == "777") {var generatedScramble = scramblers["777"].getRandomScramble().scramble_string;}
+            
+            if (scrambleType == "333bf") {var generatedScramble = scramblers["333bf"].getRandomScramble().scramble_string;}
+            
+            scrambles.push(generatedScramble);
+            scramble.textContent = generatedScramble;
+            scrambleIndex += 1;
         }
     }
 }
@@ -132,7 +199,6 @@ document.body.onkeydown = function(e) {
         if (!watch.isOn) {
             scrambleIndex -= 1;
             scramble.textContent = scrambles[scrambleIndex];
-            console.log(scrambleIndex);
         }
     }
 
@@ -141,7 +207,6 @@ document.body.onkeydown = function(e) {
         if (!watch.isOn) {
             scrambleIndex += 1;
             scramble.textContent = scrambles[scrambleIndex];
-            console.log(scrambleIndex);
         }
     }
 
@@ -155,19 +220,15 @@ document.body.onkeydown = function(e) {
     }
 
     if (e.keyCode == 8 && solves.length > 0) {
-<<<<<<< HEAD
         if (!watch.isOn) {
             var delConfirm = confirm("Delete last solve?");
-=======
-        var delConfirm = confirm("Delete last solve?");
->>>>>>> 3a5e8f8e71c2416e92f5c43a29f52dfb92c45177
 
             if (delConfirm == true) {
                 solves.pop();
                 ao5s.pop();
                 ao12s.pop();
 
-                var row = table.deleteRow(2);
+                var row = table.deleteRow(1);
                 timer.textContent = "0.00";
 
                 localStorage.setItem("solves", JSON.stringify(solves));
